807cf30ac935bb45f3d16d1f685f50d2c97f230d
aquaragon <aiden.duval@gmail.com>
2022-11-18 19:48:59 -0800

    Showing max how much I love him

--- Assignments/A6/6-3.c
+++ Assignments/A6/6-3.c
@@ -14,9 +14,15 @@ float* createArray(int size) {
 	float *a = (float *)malloc(size * sizeof(float));
 	for (int i = 0; i < size; ++i) {
 		// 50% chance that a[i] is 0.0f, random value on the range
-		// // [0.76, 1.26] otherwise.
-		// float r = rand()/(float)RAND_MAX;
-		// a[i] = r < 0.5f ? 0.0f : r + 0.26f;
-		// }
+		// [0.76, 1.26] otherwise.
+		float r = rand()/(float)RAND_MAX;
+		a[i] = r < 0.5f ? 0.0f : r + 0.26f;
+		}
 	return a;
+}
+
+int main()
+{
+
+	return 0;
 }
