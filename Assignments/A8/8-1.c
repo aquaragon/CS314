a062c320b26883d3be50bfcc5d03210d8181888a
aquaragon <aiden.duval@gmail.com>
2022-12-04 18:02:23 -0800

    assignment complete

--- Assignments/A8/8-1.c
+++ Assignments/A8/8-1.c
@@ -7,7 +7,7 @@ struct Line {
 	unsigned char valid; // valid bit
 };
 
-	struct Cache {
+struct Cache {
 	struct Line *lines;
 	int numLines;
 };
@@ -28,6 +28,16 @@ struct Cache* mallocCache(int numLines) {
 	// TODO - malloc a pointer to a struct Cache, malloc a pointer to an array
 	// of struct Line instances (array length is numLines). Also initialize
 	// valid to 0 for each struct Line. Return the struct Cache pointer.
+	//
+	// allocates memory for cache struct and the array to the structs of line
+	struct Cache * newCache = malloc(sizeof(struct Cache)*numLines);
+       	newCache -> lines = malloc(sizeof(struct Line) * numLines);
+	for (int i = 0; i < numLines; i++)
+	{
+		newCache->lines[i].valid = 0;
+	}
+	newCache -> numLines = numLines;
+	return newCache;
 }
 
 void freeCache(struct Cache *cache) {
@@ -42,6 +52,17 @@ void printLine(struct Line *line, unsigned int set) {
 
 void printCache(struct Cache *cache) {
 	// TODO - print all valid lines in the cache.
+	//
+	// iterates through and checks valid bit for 1 and prints if so
+	int lineCounter = 0;
+	while(lineCounter < cache->numLines)
+	{
+		if (cache->lines[lineCounter].valid == 1)
+		{
+			printLine(&cache->lines[lineCounter],lineCounter);
+		}
+		lineCounter++;
+	}
 }
 
 void readValue(struct Cache *cache, unsigned int address) {
@@ -49,6 +70,31 @@ void readValue(struct Cache *cache, unsigned int address) {
 	// If found, indicate a hit and print the byte. If not found, indicate
 	// a miss due to either an invalid line (cold miss) or a tag mismatch
 	// (conflict miss).
+	printf("Looking for set: %x - tag: %x\n", getSet(address), getTag(address));
+ 	for(int i = 0; i < cache->numLines; i++)
+	{
+		//Case of a hit checks valid tag and set should be correct
+		if (cache->lines[i].valid == 1 && cache->lines[i].tag == getTag(address) && i == getSet(address)) 
+		{
+				printf("Found set: ");
+			       	printLine(&cache->lines[i], i);
+				printf("Hit!\n");
+				i = cache->numLines;
+		}
+		else
+		{	//case of a conflict miss checks tag not being correct but valid bit and set are
+			if(cache->lines[i].valid == 1 && i == getSet(address) && cache->lines[i].tag != getTag(address))
+			{
+				printf("Miss\n");
+				i = cache->numLines;
+			}//case of a cold miss, valid is zero tag and set should be correct
+			else if(cache->lines[i].valid == 0 && cache->lines[i].tag == getTag(address) && i == getSet(address))
+			{
+				printf("Miss\n");
+				i = cache->numLines;
+			}
+		}
+	}
 }
 
 void writeValue(struct Cache *cache, unsigned int address, unsigned char *newData) {
@@ -74,37 +120,38 @@ unsigned int readUnsignedIntFromHex() {
 	char *p = NULL;
 	unsigned int n;
 	while (1) {
-	fgets(buffer, sizeof(buffer), stdin);
-n = strtoul(buffer, &p, 16);
-if (buffer != p) {
-break;
+		fgets(buffer, sizeof(buffer), stdin);
+		n = strtoul(buffer, &p, 16);
+		if (buffer != p) {
+			break;
+		}
+		printf("Invalid input - try again: ");
+	}
+	return n;
 }
-printf("Invalid input - try again: ");
-}
-return n;
-}
+
 int main() {
-struct Cache *cache = mallocCache(8);
-char buffer[10];
-char c;
-do {
-printf("Enter 'r' for read, 'w' for write, 'p' to print, 'q' to quit: ");
-fgets(buffer, sizeof(buffer), stdin);
-c = buffer[0];
-if (c == 'r') {
-printf("Enter 32-bit unsigned hex address: ");
-unsigned int a = readUnsignedIntFromHex();
-readValue(cache, a);
-} else if (c == 'w') {
-printf("Enter 32-bit unsigned hex address: ");
-unsigned int a = readUnsignedIntFromHex();
-printf("Enter 32-bit unsigned hex value: ");
-unsigned int v = readUnsignedIntFromHex();
-unsigned char *data = (unsigned char *)&v;
-writeValue(cache, a, data);
-} else if (c == 'p') {
-printCache(cache);
-}
-} while (c != 'q');
-freeCache(cache);
+	struct Cache *cache = mallocCache(8);
+	char buffer[10];
+	char c;
+	do {
+		printf("Enter 'r' for read, 'w' for write, 'p' to print, 'q' to quit: ");
+		fgets(buffer, sizeof(buffer), stdin);
+		c = buffer[0];
+		if (c == 'r') {
+			printf("Enter 32-bit unsigned hex address: ");
+			unsigned int a = readUnsignedIntFromHex();
+			readValue(cache, a);
+		} else if (c == 'w') {
+			printf("Enter 32-bit unsigned hex address: ");
+			unsigned int a = readUnsignedIntFromHex();
+			printf("Enter 32-bit unsigned hex value: ");
+			unsigned int v = readUnsignedIntFromHex();
+			unsigned char *data = (unsigned char *)&v;
+			writeValue(cache, a, data);
+		} else if (c == 'p') {
+			printCache(cache);
+		}
+	} while (c != 'q');
+	freeCache(cache);
 }
